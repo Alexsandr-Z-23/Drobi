9810554fce572b6af2037bd0c65b8bd02cc54690
Alexsandr <alex.zelik.m@bk.ru>
2023-07-14 18:52:27 +0300

    Добавьте файлы проекта.

---
+++ Drobi/Program.cs
@@ -0,0 +1,32 @@
+﻿using Drobi;
+
+
+
+int Den1, Num1, Den2, Num2;
+Console.WriteLine("Введите числитель первой дроби");
+Num1= Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Введите знаменатель первой дроби");
+Den1 = Convert.ToInt32(Console.ReadLine());
+Tdrob d1 = new Tdrob { num = Num1, denum=Den1 };
+Console.WriteLine($"**************************************");
+
+Console.WriteLine("Введите числитель второй дроби");
+Num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите знаменатель второй дроби");
+Den2 = Convert.ToInt32(Console.ReadLine());
+Tdrob d2 = new Tdrob { num = Num2, denum = Den2 };
+Console.WriteLine($"\n**************************************");
+
+d1.Print1();
+d2.Print2();
+Console.WriteLine($"\n**************************************");
+Tdrob d3 =new Tdrob { num=5 };
+d3.Print1();
+Console.WriteLine($"\n**************************************");
+Tdrob C1 = new Tdrob {  };
+C1 = d1 + d2;
+
+C1.Print();
+Console.WriteLine($"**************************************");
+(int num, int dnum) = C1;
+Console.WriteLine($"Числитель => {num};\nЗнаменатель => {dnum};");
---
+++ Drobi/Tdrob.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Drobi
+{
+    internal class Tdrob
+    {
+        public int num;
+        public int denum;
+
+        public Tdrob() : this(0)
+        {
+            num = 0;
+            denum = 1;
+           
+        }
+        public Tdrob(int num) : this(num, 1) { }
+        public Tdrob(int num, int denum)
+        {
+            this.num = num;
+            this.denum = denum;
+        }
+
+        public void Deconstruct(out int Dnum, out int Ddenum)
+        {
+            Dnum = num;
+            Ddenum = denum;
+        }
+        public void Print1() => Console.Write($"\nДробь [1]  => : {num} /{denum} ");
+        public void Print2() => Console.Write($"\nДробь [2]  => : {num} /{denum} ");
+        public void Print() => Console.Write($"\nДробь итог  => :{num}/{denum} => {(double) num/denum} \n");
+
+        public static Tdrob operator +(Tdrob a, Tdrob b)
+        {
+            Tdrob t = new Tdrob();
+            t.num = (a.num * b.denum + a.denum * b.num);
+            t.denum = a.denum * b.denum;
+            return t;
+
+        }
+        public static Tdrob operator -(Tdrob a, Tdrob b)
+        {
+            Tdrob t = new Tdrob();
+            t.num = (a.num * b.denum - a.denum * b.num);
+            t.denum = a.denum * b.denum;
+            return t;
+
+        }
+        public static Tdrob operator *(Tdrob a, Tdrob b)
+        {
+            Tdrob t = new Tdrob();
+            t.num = (a.num * b.num);
+            t.denum = a.denum * b.denum;
+            return t;
+
+        }
+        public static Tdrob operator /(Tdrob a, Tdrob b)
+        {
+            Tdrob t = new Tdrob();
+            t.num = (a.num / b.num);
+            t.denum = a.denum / b.denum;
+           
+            return t;
+        }
+
+        
+
+
+    }
+}
